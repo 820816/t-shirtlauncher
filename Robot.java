266d373060785deb09ba4da3ff0fe09d458938ef
820816 <44202558+820816@users.noreply.github.com>
2019-04-30 19:02:42 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -9,18 +9,16 @@
 package frc.robot;
 
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.JoystickButton;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive
@@ -36,30 +34,32 @@ public class Robot extends TimedRobot {
 
   private Spark m_frontLeft;
   private Spark m_frontRight;
-  //private Spark m_rearLeft;
   private Victor m_rearLeft;
   private Spark m_rearRight;
   private MecanumDrive m_robotDrive;
   private Joystick m_stick;
+  private JoystickButton toggleButton, led1, led2, led3;
+  private double ledStyle = 0;
   private DoubleSolenoid launchersolenoid;
   private DigitalInput getPressureSwitchValue;
   private Relay compressorSwitch;
   private Spark m_led_driver;
+  private Talon feeder;
+  private JoystickButton feederbutton; 
 
-
-
+/**DeadBand Code */
   private static double linearDeadband(double raw, double deadband)
- {
+  {
  
      if (Math.abs(raw)<deadband) return 0;
  
     return Math.signum(raw)*(Math.abs(raw)-deadband)/(1-deadband);
- }
+  }
 
+
+/**Innit */
   @Override
   public void robotInit() {
-
-
     
     m_frontLeft = new Spark(0);	
     m_rearLeft = new Victor(4);
@@ -68,43 +68,71 @@ public class Robot extends TimedRobot {
     m_led_driver = new Spark(5);
     m_robotDrive = new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
     m_stick = new Joystick(kJoystickChannel);
-    launchersolenoid = new DoubleSolenoid(1,0,1);
+    toggleButton = new JoystickButton(m_stick, 1);
+    led1 = new JoystickButton(m_stick, 2);
+    led2 = new JoystickButton(m_stick, 3);
+    led3 = new JoystickButton(m_stick, 4);
+    launchersolenoid = new DoubleSolenoid(1,0);
     getPressureSwitchValue = new DigitalInput(2);
     compressorSwitch = new Relay(1);
+    feeder = new Talon(6);
+    feederbutton = new JoystickButton(m_stick,5);
     
 
     // Invert the left side motors.
     // You may need to change or remove this to match your robot.
     m_frontLeft.setInverted(false);
     m_rearLeft.setInverted(true);
+   //m_robotDrive.setDeadband(0.05);
+
   }
-
-  
   //Override
-  
 
-  public void teleopPeriodic() {
+/**Teleop Period */
+public void teleopPeriodic() {
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
     m_robotDrive.driveCartesian(m_stick.getZ(), m_stick.getY(), m_stick.getX(), 0.0);
 
-
-    //launchersolenoid.set(DoubleSolenoid.Value.kOff);
-    //launchersolenoid.set(DoubleSolenoid.Value.kForward);
-    //launchersolenoid.set(DoubleSolenoid.Value.kReverse);
-
-    if(m_stick.getRawButton(1)) {
-      launchersolenoid.set(DoubleSolenoid.Value.kForward);
-    } else {
-      launchersolenoid.set(DoubleSolenoid.Value.kReverse);
-    }
-
-   // if(m_stick.getrawButton(3)) {
-   //   toggleWhenPressed(m_led_driver.set(.5));
-   // } 
-
-   
-
+    ledFunction();
+    feeder();
+    launch();
+    
   }
+
+
+  /** This is the function that checks buttons and sets the LEDs accordingly */
+  private void ledFunction() {
+    if (led1.get() && ledStyle != .63 /* This is some value */) ledStyle = .63;
+    else if (led1.get()) ledStyle = 0;
+    if (led2.get() && ledStyle != -.11/* This is some value */) ledStyle = -.11;
+    else if (led2.get()) ledStyle = 0;
+    if (led3.get() && ledStyle != .45/* This is some value */) ledStyle = .45;
+    else if (led3.get()) ledStyle = 0;
+
+    m_led_driver.set(ledStyle);
+
+    
+  }
+
+/**This function controls the feeder belt */
+  private void feeder() {
+    if (feederbutton.get()) {
+      feeder.set(.5);
+    } else {
+      feeder.set(0);
+    }
+  }
+
+/**This function controls the launch mechananism  */
+  private void launch() {
+    if (toggleButton.get()) {
+    launchersolenoid.set(DoubleSolenoid.Value.kForward);
+   } else {
+    launchersolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+  
+  }
+
 }
 
